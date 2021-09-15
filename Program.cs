f502d432e15441536c7863346e110203650e62d3
mitali2608 <mitalibawankule21@gmail.com>
2021-09-15 16:35:47 +0530

    Part 2 of EmployeeWage added

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace EmployeeWage2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UC1_EmployeeAttendance E1 = new UC1_EmployeeAttendance();
+            E1.GetAttendance();
+            UC2_EmpWage E2 = new UC2_EmpWage();
+            E2.EmpWage();
+            UC3_PartTimeEmpWage E3 = new UC3_PartTimeEmpWage();
+            E3.PartTimeEmpWage();
+            UC4_EmpWageUsingSwitch E4 = new UC4_EmpWageUsingSwitch();
+            E4.EmpWageUsingSwitch();
+            UC5_CalculateWagesFor20DaysInMonth E5 = new UC5_CalculateWagesFor20DaysInMonth();
+            E5.CalculateWagesFor20DaysInMonth();
+            UC6_CalculateWageTill100HrsOr20IsReached E6 = new UC6_CalculateWageTill100HrsOr20IsReached();
+            E6.CalculateWageTill100HrsOr20IsReached();
+            UC7_ComputeEmpWageUsingClassMethod E7 = new UC7_ComputeEmpWageUsingClassMethod();
+            E7.ComputeEmpWage();
+            UC8_ComputeEmpWageForMultipleCompanies E8 = new UC8_ComputeEmpWageForMultipleCompanies();
+            E8.ComputeEmpWage("xyz", 10, 2, 8);
+            Console.ReadKey();
+        }
+
+      
+    }
+}
---
+++ UC1_EmployeeAttendance.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWage2
+
+{
+    class UC1_EmployeeAttendance
+    {
+        public void GetAttendance()
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+                Console.WriteLine("Employee is present");
+            else
+                Console.WriteLine("Employee is absent");
+        }
+
+    }
+}
